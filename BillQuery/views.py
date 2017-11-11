cb6485d60fc94acbe9fbd90278ad548eb88ac2f4
ser_kenny <ser_kenny@outlook.com>
2017-11-12 00:16:55 +0800

    修改表格模板。
    Signed-off-by: ser_kenny <ser_kenny@outlook.com>

--- BillQuery/views.py
+++ BillQuery/views.py
@@ -4,9 +4,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 from Authentication.decorators import required_login
+from Authentication.models import Phone
 
 
-# 暂时没有帐单数据，只能使用账户数据
+# 暂时没有帐单数据，只能使用账户数据；通过 127.0.0.1:8000/bill 访问
+# 模板来自 git@github.com:bopoda/ace.git
 @required_login
 def stub(request):
-    return render(request, 'BillQuery/page.html')
+    context = {
+        "phone_collection": Phone.objects.all()
+    }
+    return render(request, 'BillQuery/page.html', context)
