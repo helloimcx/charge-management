5e9ad9c37d698c26468ae5dc41e6d733f8a765c6
ser_kenny <ser_kenny@outlook.com>
2017-11-12 21:47:14 +0800

    避免重复插入帐单项目。
    changes made to ea20b262752e2647d8e7a8bd763f9e5874734578.
    
    Signed-off-by: ser_kenny <ser_kenny@outlook.com>

--- BillQuery/management/commands/make_item.py
+++ BillQuery/management/commands/make_item.py
@@ -1,6 +1,7 @@
 from django.core.management import BaseCommand
 from BillQuery.models import Item
 from io import StringIO
+from django.db.utils import IntegrityError
 
 
 class Command(BaseCommand):
@@ -9,10 +10,13 @@ class Command(BaseCommand):
         with open(src, 'rb') as f:
             content = f.read().decode(encoding='utf-8')
         f = StringIO(content)
-        for line in f:
-            name, p = line.strip().split()
-            price = float(p)
-            Item.objects.create(name=name, price=price)
+        try:
+            for line in f:
+                name, p = line.strip().split()
+                price = float(p)
+                Item.objects.create(name=name, price=price)
+        except IntegrityError:
+            print('This command should be executed only once.')
         f.close()
 
 
